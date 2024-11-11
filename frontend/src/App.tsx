8aef5d7a26988641b0b9da2ea0c4acf968db3677
Quang Huy <quanghuyy150604@gmail.com>
2024-11-11 13:19:47 +0700

    Create NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,32 +2,21 @@ import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { testAPI } from './api/test';
+import NavBar from './components/NavBar/NavBar';
+
 
 function App() {
-  useEffect(() => {
-    testAPI().then((res) => {
-      console.log(res);
-    })
-  }, [])
+  // useEffect(() => {
+  //   testAPI().then((res) => {
+  //     console.log(res);
+  //   })
+  // }, [])
+
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <NavBar />
     </div>
   );
 }
-
 export default App;
--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 
-type Props = {}
+type Props = {};
 
-const NavBar = (props: Props) => {
+const NavBar: React.FC<Props> = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav>
+      <div id="logo">
+        <Link to="/" style={{ display: 'block'}}>
+          <span style={{ fontSize: '1.5rem' }}>AHA AIRLINE</span> <br/>
+          <span style={{ fontSize: '0.7rem' }}>FLYING WITHOUT WINGS</span>
+        </Link>
+      </div>
+      <div id="nav">
+        <Link to="/booking">Book Flight</Link>
+        <Link to="/explore">Explore</Link>
+        <Link to="/travelInformation">Travel Information</Link>
+        <Link to="/experience">Experience</Link>
+      </div>
+      <div id="account">
+        <Link to="/login">Login</Link>
+        <Link to="/sign-up">Sign Up</Link>
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
