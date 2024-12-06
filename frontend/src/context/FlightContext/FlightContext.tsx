fd4dcbaf477d6b8e9e544883baca18bf622b3681
gnqHuy <quanghuyy150604@gmail.com>
2024-12-07 03:03:01 +0700

    Create: Many Things

--- frontend/src/context/FlightContext/FlightContext.tsx
+++ frontend/src/context/FlightContext/FlightContext.tsx
@@ -1,18 +1,33 @@
 import React, { createContext, useContext, useState } from "react";
 import { Flight } from "../../object/flight/flight";
+import { PassengerCount } from "../../object/passengerCount/passengerCount";
 
 type FlightContextType = {
   selectedFlight: Flight | null;
   setSelectedFlight: (flight: Flight) => void;
+  selectedPassenger: PassengerCount;
+  setSelectedPassenger: (passengerCount: PassengerCount) => void;
 };
 
 const FlightContext = createContext<FlightContextType | undefined>(undefined);
 
 export const FlightProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [selectedFlight, setSelectedFlight] = useState<Flight | null>(null);
+  const [selectedPassenger, setSelectedPassenger] = useState<PassengerCount>({
+    adults: 1,
+    children: 0,
+    infants: 0,
+  });
 
   return (
-    <FlightContext.Provider value={{ selectedFlight, setSelectedFlight }}>
+    <FlightContext.Provider
+      value={{
+        selectedFlight,
+        setSelectedFlight,
+        selectedPassenger,
+        setSelectedPassenger,
+      }}
+    >
       {children}
     </FlightContext.Provider>
   );
