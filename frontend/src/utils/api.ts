8724b4a20e7b5dd42e0f9d21abf8e78fd472dfae
dwcsnh <ducanhdao0411@gmail.com>
2024-12-19 20:50:57 +0700

    Add redux

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,10 +1,33 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosRequestConfig, InternalAxiosRequestConfig } from "axios";
 import config from "../config";
+import { EnhancedStore } from "@reduxjs/toolkit";
 
 const axiosInstance = axios.create({
     baseURL: config.API_URL,
     responseType: 'json',
     timeout: 20000,
 });
+
+export const setupAxios = (store: EnhancedStore) => {
+    axiosInstance.interceptors.request.use(
+        (request: InternalAxiosRequestConfig) => {
+            const {
+                auth: { accessToken },
+            } = store.getState();
+
+            if (accessToken) {
+                if (request.headers) {
+                    request.headers.set("Authorization", `Bearer ${accessToken}`);
+                    
+                }
+            }
+            console.log(request.headers);
+
+            return request;
+        },
+        (error: AxiosError) => Promise.reject(error)
+    );
+
+}
 
 export default axiosInstance;
